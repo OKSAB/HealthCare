f85dd1f111b92214540f7055b43d094d65823574
Osama Bugshan <osama.bugshan@kcl.ac.uk>
2025-04-07 06:50:22 +0300

    fix asset mapping error

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+  },
+  // Map asset file extensions (e.g. png, jpg, svg) to a mock module
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation)/)'
+  ],
 };
